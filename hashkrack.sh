d9ecd08adbc160e65b717241f39582355a08a33f
Dukhi Atma <140259952+Dukh14tma@users.noreply.github.com>
2023-08-08 14:09:34 +0530

    Update hashkrack.sh

--- hashkrack.sh
+++ hashkrack.sh
@@ -2,7 +2,9 @@
 
 # Define text formatting variables
 bold=$(tput bold)
-red=$(tput setaf 1)                                                                                                                                                               green=$(tput setaf 2)                                                                                                                                                             reset=$(tput sgr0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+reset=$(tput sgr0)
 
 # Display logo
 logo="
@@ -15,29 +17,24 @@ logo="
 | (   ) || (   ) |      ) || (   ) ||  ( \\ \\ | (\\ (   | (   ) || |      |  ( \\ \\
 | )   ( || )   ( |/\\____) || )   ( ||  /  \\ \\| ) \\ \\__| )   ( || (____/\\|  /  \ \
 
-|/     \\||/     \\|\\_______)|/     \\||_/    \\/|/   \\__/|/     \\|(_______/|_/    \\/
-                                                              version 0.1
-"
-author="
-==================================================================================
-============================= Created by Dukh14tma ===============================
-==================================================================================
+|/     \\||/     \\|\\_______)|/     \\||_/    \\/|/   \\__/|
 
-"
-clear                                                                                                                                                                             
 # Print logo
 echo "${bold}${green}$logo${reset}"
-echo "${bold}${red}$author${reset}"                                                                                                                                                                                                                                                                                                                                 # Prompt user for input
+echo
+# Prompt user for input
 read -p "${bold}${red}[+]${green}    Enter the hash to crack: " hash_to_crack
-echo                                                                                                                                                                              
-read -p "${bold}${red}[+]${green}    Enter the path to the wordlist file: " wordlist ${reset}                                                                                                                                                                                                                                                                       
-# List of hashing algorithms                                                                                                                                                      
+echo
+read -p "${bold}${red}[+]${green}    Enter the path to the wordlist file: " wordlist ${reset}
+
+# List of hashing algorithms
 algorithms=("md5" "sha1" "sha224" "sha256" "sha384" "sha512" "ripemd160" "whirlpool" "tiger128,3" "tiger160,3" "tiger192,3" "tiger128,4" "tiger160,4" "tiger192,4" "snefru" "gost" "adler32" "crc32" "crc32b" "fnv132" "fnv164" "joaat" "haval128,3" "haval160,3" "haval192,3" "haval224,3" "haval256,3" "haval128,4" "haval160,4" "haval192,4" "haval224,4" "haval256,4" "haval128,5" "haval160,5" "haval192,5" "haval224,5" "haval256,5")
 
 # Loop through algorithms and wordlist
 for algorithm in "${algorithms[@]}"; do
   while read -r password; do
-    case $algorithm in                                                                                                                                                                  "md5")
+    case $algorithm in
+      "md5")
         hash=$(echo -n "$password" | md5sum | awk '{print $1}')
         ;;
       "sha1")
@@ -53,7 +50,8 @@ for algorithm in "${algorithms[@]}"; do
         hash=$(echo -n "$password" | sha384sum | awk '{print $1}')
         ;;
       "sha512")
-        hash=$(echo -n "$password" | sha512sum | awk '{print $1}')                                                                                                                        ;;
+        hash=$(echo -n "$password" | sha512sum | awk '{print $1}')
+        ;;
       "ripemd160")
         hash=$(echo -n "$password" | openssl dgst -ripemd160 | awk '{print $2}')
         ;;
@@ -61,9 +59,9 @@ for algorithm in "${algorithms[@]}"; do
         hash=$(echo -n "$password" | openssl dgst -whirlpool | awk '{print $2}')
         ;;
       "tiger128,3" | "tiger160,3" | "tiger192,3" | "tiger128,4" | "tiger160,4" | "tiger192,4")
-        hash=$(echo -n "$password" | openssl dgst "-$algorithm" | awk '{print substr($0, length($0)-31)}')                                                                                ;;
-      "snefru" | "gost" | "adler32" | "crc32" | "crc32b" | "fnv132" | "fnv164" | "joaat" | "haval128,3" | "haval160,3" | "haval192,3" | "haval224,3" | "haval256,3" | "haval128,4"
- | "haval160,4" | "haval192,4" | "haval224,4" | "haval256,4" | "haval128,5" | "haval160,5" | "haval192,5" | "haval224,5"| "haval256,5")
+        hash=$(echo -n "$password" | openssl dgst "-$algorithm" | awk '{print substr($0, length($0)-31)}')
+        ;;
+      "snefru" | "gost" | "adler32" | "crc32" | "crc32b" | "fnv132" | "fnv164" | "joaat" | "haval128,3" | "haval160,3" | "haval192,3" | "haval224,3" | "haval256,3" | "haval128,4" | "haval160,4" | "haval192,4" | "haval224,4" | "haval256,4" | "haval128,5" | "haval160,5" | "haval192,5" | "haval224,5"| "haval256,5")
         echo "${red}The algorithm '$algorithm' is not supported without OpenSSL. Please remove it from the 'algorithms' array to continue.${reset}"
         continue
         ;;
